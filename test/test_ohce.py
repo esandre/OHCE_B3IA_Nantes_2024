9f6623445b96ad0849264ebead0e3d7f88a8a12d
Enzo Sandre <enzo@enzosandre.fr>
2024-05-03 10:36:40 +0200

    [REFACTO] Fusion des 2 tests

--- test/test_ohce.py
+++ test/test_ohce.py
@@ -5,25 +5,17 @@ from verificateur_palindrome import VérificateurPalindrome
 
 class PalindromeTest(unittest.TestCase):
     def test_miroir(self):
+        cas = ["epsi", "test"]
+
         # ETANT DONNE un non-palindrome
-        chaîne = "epsi"
+        for chaîne in cas:
+            with self.subTest(chaîne):
+                # QUAND on vérifie si c'est un palindrome
+                résultat = VérificateurPalindrome.vérifier(chaîne)
 
-        # QUAND on vérifie si c'est un palindrome
-        résultat = VérificateurPalindrome.vérifier(chaîne)
-
-        # ALORS la chaîne est renvoyée en miroir
-        self.assertEqual("ispe", résultat)
-
-
-    def test_miroir2(self):
-        # ETANT DONNE un non-palindrome
-        chaîne = "test"
-
-        # QUAND on vérifie si c'est un palindrome
-        résultat = VérificateurPalindrome.vérifier(chaîne)
-
-        # ALORS la chaîne est renvoyée en miroir
-        self.assertEqual("tset", résultat)
+                # ALORS la chaîne est renvoyée en miroir
+                attendu = chaîne[::-1]
+                self.assertEqual(attendu, résultat)
 
 
 if __name__ == '__main__':
